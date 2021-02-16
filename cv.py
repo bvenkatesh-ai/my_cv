276ab116267c28e8f8959619926b2bb5a41cd37e
bvenkatesh-ai <56408803+bvenkatesh-ai@users.noreply.github.com>
2021-02-16 12:31:09 +0530

    Update cv.py

--- cv.py
+++ cv.py
@@ -17,6 +17,8 @@ def main():
     st.sidebar.markdown("---------------")
     if sel_page == pages[0]:
         st.subheader(pages[0])
+        image = Image.open('bvenkatesh_cv.png')
+        st.image(image, caption="Boddu Venkatesh", width=130)
   #      num = len(my_skills[0]['Project Name'])
    #     cols = st.beta_columns(num)
     #    p= [1,2,3,4]
