55e8a4422b633dbee1843dd4ae9823986d856061
bvenkatesh-ai <56408803+bvenkatesh-ai@users.noreply.github.com>
2021-02-16 17:53:50 +0530

    Update cv.py

--- cv.py
+++ cv.py
@@ -28,7 +28,7 @@ def main():
                base64_pdf = base64.b64encode(f.read()).decode('utf-8')
                pdf_display = f"""<embed src="data:application/pdf;base64,{base64_pdf}" width="400" height="600" type="application/pdf">"""
                st.markdown(pdf_display, unsafe_allow_html=True)
-         if sel == "Image":
+        if sel == "Image":
             image = Image.open('bvenkatesh_cv.png')
             st.image(image, caption="Boddu Venkatesh", use_column_width=True)
   #      num = len(my_skills[0]['Project Name'])
